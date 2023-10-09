67590395d0e80cb97ffa9906b13e5d90136f8270
MattyTheHacker <18513864+MattyTheHacker@users.noreply.github.com>
2023-10-10 00:17:07 +0100

    fix list comprehension issue

--- src/main.py
+++ src/main.py
@@ -5,6 +5,6 @@ get_all_election_data()
 
 files = get_all_data_file_names('../data/json/raw/')
 
-data = load_json_data('../data/json/raw/' + files[0])
+data = load_json_data('../data/json/raw/' + files[-1])
 
 save_to_db(data, get_generated_date(data))
