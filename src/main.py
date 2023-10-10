7ab6d8577513facdc0f1a7cc23c8a97b66e08cfb
MattyTheHacker <18513864+MattyTheHacker@users.noreply.github.com>
2023-10-10 13:58:55 +0100

    update file handling

--- src/main.py
+++ src/main.py
@@ -5,6 +5,12 @@ get_all_election_data()
 
 files = get_all_data_file_names('../data/json/raw/')
 
-data = load_json_data('../data/json/raw/' + files[0])
+# files names are in the format: YYYY-MM-DDTHH-MM-SS.json
+# we want to sort by date, so we need to extract the date from the file name
+# and then sort by that
+files.sort(key=lambda x: datetime.strptime(x.split('.')[0], '%Y-%m-%dT%H-%M-%S'))
+
+# we want to load the most recent data
+data = load_json_data('../data/json/raw/' + files[-1])
 
 save_to_db(data, get_generated_date(data))
