cd42d6e40db8b901fb478b62f6c200a8535e22c7
MattyTheHacker <18513864+MattyTheHacker@users.noreply.github.com>
2023-10-10 14:02:09 +0100

    fix time conversion

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ files = get_all_data_file_names('../data/json/raw/')
 # files names are in the format: YYYY-MM-DDTHH-MM-SS.json
 # we want to sort by date, so we need to extract the date from the file name
 # and then sort by that
-files.sort(key=lambda x: datetime.strptime(x.split('.')[0], '%Y-%m-%dT%H-%M-%S'))
+files.sort(key=lambda x: datetime.strptime(x.split('.')[0], '%Y-%m-%dT%H%M%S'))
 
 # we want to load the most recent data
 data = load_json_data('../data/json/raw/' + files[-1])
